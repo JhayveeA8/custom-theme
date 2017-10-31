238dffaeedb9a217e7fc8a3c8d65c7ae1c97202b
JhayveeA8 <33220534+JhayveeA8@users.noreply.github.com>
2017-10-31 17:37:01 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,14 +1,6 @@
 <?php
 /**
- * The template for displaying the footer.
- *
- * Contains the closing of the #content div and all content after
- *
- *
- * NONE
- *
- * @package cad-wp-theme
- * @author marcelbadua
+HELLO
  */
 ?>
     
